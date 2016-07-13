66480ceddd301b06423022728b37333fdebf27ff
Jonathan Flowers <jonny_flowers@me.com>
2016-07-13 13:51:38 +0100

    removed all the example lockbox stuff

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-// Setup basic express server
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 8080;
 
-server.listen(port, function () {
-	console.log('Server listening at port %d', port);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-// Routing
-app.use(express.static(__dirname + '/public'));
-
-var safebox = require('./models/safebox')(io);
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!');
+});
