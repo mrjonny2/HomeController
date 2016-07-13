d8a3cd1f6b75bdf262b163f2aea6790d5bcfd050
Jonathan Flowers <jonny_flowers@me.com>
2016-07-13 14:56:13 +0100

    added journaling to mongo

--- start.sh
+++ start.sh
@@ -12,4 +12,4 @@ nohup socat tcp-listen:80,fork tcp:localhost:8443 > socat.log 2>&1 &
 
 rm /datadb/mongod.lock
 /usr/bin/mongod --dbpath /datadb --repair
-/usr/bin/mongod --dbpath /datadb --fork --logpath mongod.log && node index.js
+/usr/bin/mongod --dbpath /datadb --fork --journal --logpath mongod.log && node index.js
